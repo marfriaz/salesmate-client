75f0c5ad8c71f77ad94056e1fcfcc282949607e6
Marco Friaz <marfriaz@gmail.com>
2020-10-07 22:48:14 -0700

    updated

--- src/routes/AccountPage/AccountPage.js
+++ src/routes/AccountPage/AccountPage.js
@@ -4,6 +4,7 @@ import GymApiService from "../../services/gym-api-service";
 import { Section } from "../../components/Utils/Utils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import CardItem from "../../components/CardItem/CardItem";
 
 import "./AccountPage.css";
 
@@ -27,15 +28,52 @@ export default class AccountPage extends Component {
   //   this.context.clearGym();
   // }
 
+  // renderAccountList(accountInfo) {
+  //   for (let i = 0; i < accountInfo.length; i++) {
+  //     var result = Object.entries(accountInfo[i]).map(([key, value]) => (
+  //       <AccountPageCardItems key={key} value={value} />
+  //     ));
+  //   }
+  //   return result;
+  // }
+
+  renderAccountList(accountInfo) {
+    return accountInfo.map((account, key) => (
+      <CardItem key={account.key} name={account.name} value={account.value} />
+    ));
+  }
+
   render() {
     const { account, error } = this.props;
+    const businessDetails = [
+      { name: "Account Name", value: account.name },
+      { name: "Stage", value: account.stage },
+      { name: "Website", value: account.website },
+      { name: "Phone Number", value: account.phone },
+      { name: "Fax", value: account.fax },
+      { name: "Industry", value: account.industry },
+      { name: "Territory", value: account.territory },
+      { name: "Employee Range", value: account.employee_range },
+      { name: "LI CO Page", value: account.licopage },
+    ];
+    const address = [
+      { name: "Billing", value: account.country },
+      { name: "Shipping", value: account.country },
+    ];
+
+    const contacts = [
+      { name: "Employee Name", value: account.contact.name },
+      { name: "Employee Title", value: account.contact.title },
+      { name: "Phone", value: account.contact.phone },
+      { name: "Email", value: account.contact.email },
+    ];
 
     return (
       <>
         <div className="AccountPage">
           <form className="HostGymForm" onSubmit={this.handleSubmit}>
             <div className="AccountPage__header">
-              <div className="Account__photo">
+              {/* <div className="Account__photo">
                 {" "}
                 <img
                   className=""
@@ -43,11 +81,12 @@ export default class AccountPage extends Component {
                   src={account.image_url}
                   width="100px"
                 />
-              </div>
+              </div> */}
               <div className="Account__name">
                 <h1>{account.name}</h1>
               </div>
             </div>
+
             <div className="AccountPage__buttons">
               <button className="Account__lead">Mark As Lead</button>
               <button className="Account__not_interested">
@@ -55,50 +94,31 @@ export default class AccountPage extends Component {
               </button>
               <button className="Account__sold">Mark as SOLD</button>
             </div>
-            <div className="AccountPage__contact">
-              <div className="Account__contact__header">Contact Info</div>
-              <div className="Account__contact__content">
-                <div className="AccountPage__div">
-                  <div className="Account__website">
-                    Website: {account.url.split("?")[0]}
-                  </div>
-                  <div className="Account__phone">
-                    Phone: {account.display_phone} &nbsp;
-                  </div>
-                </div>
-                <div className="Account__address">
-                  Address:{" "}
-                  {account.location.display_address
-                    .toString()
-                    .split(",")
-                    .join(", ")}
-                </div>
+
+            <div className="AccountPage__card">
+              <div className="AccountPage__card__header">Business Details</div>
+              <div className="AccountPage__card__fields">
+                {this.renderAccountList(businessDetails)}
               </div>
             </div>
-            <div className="AccountPage__insights">
-              <div className="Account__insights__header">Insights</div>
-              <div className="Account__insights__content">
-                <div className="AccountPage__div">
-                  <div className="Account__categories">
-                    Categories: {account.categories[0].title}
-                  </div>
-                  <div className="Account__reviewcount">
-                    Review Count: {account.review_count}
-                  </div>
-                </div>
-                <div className="AccountPage__div">
-                  <div className="Account__rating">
-                    Rating: {account.rating}
-                  </div>
-                  <div className="Account__price">
-                    Yelp Price: {account.price}
-                  </div>
-                </div>
+
+            <div className="AccountPage__card">
+              <div className="AccountPage__card__header">Address</div>
+              <div className="AccountPage__card__fields">
+                {this.renderAccountList(address)}
               </div>
             </div>
-            <div className="AccountPage__notes">
-              <div className="Account__notes__header">Notes</div>
-              <div className="Account__insights__content">
+
+            <div className="AccountPage__card">
+              <div className="AccountPage__card__header">Contacts</div>
+              <div className="AccountPage__card__fields">
+                {this.renderAccountList(contacts)}
+              </div>
+            </div>
+
+            <div className="AccountPage__card">
+              <div className="AccountPage__card__header">Notes</div>
+              <div className="AccountPage__card__fields">
                 <div className="Account__website">
                   Date:
                   <input />
