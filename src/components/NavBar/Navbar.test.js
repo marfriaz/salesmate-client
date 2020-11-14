8edce5fdc4837fe8ae80de77885a674a9acad1b2
Marco Friaz <marfriaz@gmail.com>
2020-11-13 16:11:09 -0800

    updated NavBar test

--- src/components/NavBar/Navbar.test.js
+++ src/components/NavBar/Navbar.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Navbar from "./Navbar";
+import Navbar from "../Navbar/Navbar";
 import { MemoryRouter } from "react-router-dom";
 
 describe("Navbar Component", () => {
