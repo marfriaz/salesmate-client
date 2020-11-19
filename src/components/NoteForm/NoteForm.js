cd21af766fb06095fb6179c31dd6abf72b3fb4cd
Marco Friaz <marfriaz@gmail.com>
2020-11-18 20:32:40 -0800

    updated noteform

--- src/components/NoteForm/NoteForm.js
+++ src/components/NoteForm/NoteForm.js
@@ -1,46 +1,14 @@
 import React, { Component } from "react";
-import AccountApiService from "../../services/account-api-service";
 import { Required } from "../Utils/Utils";
 import "./NoteForm.css";
 
 export default class NoteForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      note: {},
-      noteId: this.props.noteId,
-      accountId: this.props.accountId,
-      error: null,
-    };
-  }
-
-  componentDidMount() {
-    const { note } = this.props;
-    this.setState({
-      note: note,
-    });
-  }
-
-  handleSubmit = (ev) => {
-    const { accountId, noteId, text } = this.props;
-
-    ev.preventDefault();
-    this.setState({ error: null });
-    AccountApiService.postNote(accountId, text)
-      .then((note) => this.props.updateNotes(note))
-      .catch((res) => {
-        this.setState({ error: res.error });
-      });
-    this.props.updatecreateNotes(noteId);
-  };
-
   updateText(value) {
     const { noteId } = this.props;
-    let updates = { ...this.state.contact };
+    let updates = { ...this.props.note };
     Object.assign(updates, {
       text: value,
     });
-    this.setState({ note: updates });
 
     this.props.updateFields(updates, noteId);
   }
