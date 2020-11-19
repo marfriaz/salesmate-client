6e79a4c6e7fe54b45c03685619b4f3014a8aab3f
Marco Friaz <marfriaz@gmail.com>
2020-11-18 22:13:30 -0800

    updated API endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
-  // API_ENDPOINT: "https://boiling-thicket-16712.herokuapp.com/api",
+  // API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://boiling-thicket-16712.herokuapp.com/api",
   TOKEN_KEY: "salesmate-client-auth-token",
 };
